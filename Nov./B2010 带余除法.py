cba8cffbe364a9e5e86a844445049fcccde2210c
cZanY714285 <firstczhuzany@163.com>
2024-11-24 13:45:48 +0800

    Add files via upload

---
+++ Nov./B2010 带余除法.py
@@ -0,0 +1,7 @@
+a,b = input().split()
+a = int(a)
+b = int(b)
+n = int(a / b)
+m = a % b
+print(n,end = ' ')
+print(m)
---
+++ Nov./B2014 与圆相关的计算.py
@@ -0,0 +1,4 @@
+r = float(input())
+print('%.4f' % (2 * r),end = ' ')
+print('%.4f' % (2 * 3.14159 * r),end = ' ')
+print('%.4f' % (r * 3.14159 * r),end = ' ')
